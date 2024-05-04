b4fea581ec608c01913484cb4ddd428a42d2fc13
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-05-04 16:39:58 +0300

    Second Problem

--- Django Models Basics - Exercise/main_app/models.py
+++ Django Models Basics - Exercise/main_app/models.py
@@ -3,3 +3,9 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=30)
     age = models.PositiveIntegerField()
+
+
+class Blog(models.Model):
+    post = models.TextField()
+    author = models.CharField(max_length=35)
+    
