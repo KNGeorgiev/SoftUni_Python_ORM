652846d2c7aa0e30ca098a8b8e2107cdc8a0fb79
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-05-08 21:21:13 +0300

    End

--- Migrations and Django Admin - Lab/orm_skeleton/settings.py
+++ Migrations and Django Admin - Lab/orm_skeleton/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "migrations_and_django_admin",
-        "USER": "postgres-user",
-        "PASSWORD": "password",
+        "NAME": "Migrations_and_Django_Admin",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
