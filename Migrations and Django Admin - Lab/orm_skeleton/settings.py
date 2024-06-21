812a6d855099e21fea3c1426d318a6ed0109bf3a
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-06-21 23:56:56 +0300

    First exercise

--- Migrations and Django Admin - Lab/orm_skeleton/settings.py
+++ Migrations and Django Admin - Lab/orm_skeleton/settings.py
@@ -75,13 +75,20 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Migrations_and_Django_Admin",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "127.0.0.1",
+        "NAME": "migrations_and_django_admin",
+        "USER": "postgres-user",
+        "PASSWORD": "password",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
