92f06cd79f07a9cb33822a0677c56e67342aa33d
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-06-23 23:27:28 +0300

    first_problem

--- Migrations and Django Admin - Exercise/orm_skeleton/settings.py
+++ Migrations and Django Admin - Exercise/orm_skeleton/settings.py
@@ -76,16 +76,22 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "migrations_and_django_admin_exercise",
         "USER": "postgres-user",
         "PASSWORD": "password",
-        "HOST": "127.0.0.1",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
