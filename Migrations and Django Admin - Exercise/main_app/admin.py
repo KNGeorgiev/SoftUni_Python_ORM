5e0083cdd81cfb337b74dfc92b7f5e3828b952e7
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-06-29 09:16:55 +0300

    More exercises

--- Migrations and Django Admin - Exercise/main_app/admin.py
+++ Migrations and Django Admin - Exercise/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main_app.models import EventRegistration, Movie, Student, Supplier
+from main_app.models import EventRegistration, Movie, Student, Supplier, Course
 
 
 @admin.register(EventRegistration)
@@ -41,5 +41,22 @@ class SupplierAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Information', {
             'fields': ('name', 'contact_person', 'email', 'address')
+        })
+    ]
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'lecturer', 'price', 'start_date']
+    list_filter = ['is_published', 'lecturer']
+    search_fields = ['title', 'lecturer']
+    readonly_fields = ['start_date']
+    fieldsets = [
+        ('Course Information', {
+            'fields': ('title', 'lecturer', 'price', 'start_date', 'is_published')
+        }),
+
+        ('Description', {
+            'fields': ('description',)
         })
     ]
