72435de6176243cd34eb89a4c1a78adb251cc3b1
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-07-07 22:58:06 +0300

    Second

--- Data Operations in Django with Queries - Exercise/caller.py
+++ Data Operations in Django with Queries - Exercise/caller.py
@@ -6,19 +6,49 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 
-from main_app.models import Pet
+from main_app.models import Pet, Artifact
 
 
-def create_pet(name: str, species: str):
-    pet = Pet(
+# def create_pet(name: str, species: str):
+#     pet = Pet(
+#         name = name,
+#         species = species
+#     )
+#     pet.save()
+
+#     return f"{pet.name} is a very cute {pet.species}!"
+
+
+# print(create_pet('Buddy', 'Dog'))
+# print(create_pet('Whiskers', 'Cat'))
+# print(create_pet('Rocky', 'Hamster'))
+
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool):
+    artifact = Artifact(
         name = name,
-        species = species
+        origin = origin,
+        age = age,
+        description = description,
+        is_magical = is_magical
     )
-    pet.save()
+    artifact.save()
 
-    return f"{pet.name} is a very cute {pet.species}!"
+    return f"The artifact {artifact.name} is {artifact.age} years old!"
 
 
-print(create_pet('Buddy', 'Dog'))
-print(create_pet('Whiskers', 'Cat'))
-print(create_pet('Rocky', 'Hamster'))
+def rename_artifact(artifact: Artifact, new_name: str):
+    
+    if artifact.is_magical and artifact.age > 250:
+        artifact.name = new_name
+    artifact.save()
+
+
+def delete_all_artifacts():
+    Artifact.objects.all().delete()
+
+delete_all_artifacts()
+print(create_artifact('Ancient Sword', 'Lost Kingdom', 500, 'A legendary sword with a rich history', True))
+artifact_object = Artifact.objects.get(name='Ancient Sword')
+rename_artifact(artifact_object, 'Ancient Shield')
+print(artifact_object.name)
