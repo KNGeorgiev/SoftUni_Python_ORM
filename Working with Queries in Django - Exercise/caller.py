995c359cebc8b2622be6e6d3398fa0cdf087c2b5
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-07-20 23:01:29 +0300

    ChessPlayer

--- Working with Queries in Django - Exercise/caller.py
+++ Working with Queries in Django - Exercise/caller.py
@@ -10,8 +10,8 @@ django.setup()
 # Create and check models
 # Run and print your queries
 
-from main_app.models import ArtworkGallery, Laptop
-from django.db.models import Case, When, Value
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer
+from django.db.models import Case, When, Value, Q
 
 ###########################################################################
 
@@ -38,33 +38,70 @@ from django.db.models import Case, When, Value
 
 ###########################################################################
 
-def show_the_most_expensive_laptop():
-    most_expensive = Laptop.objects.order_by('-price', '-id').first()
-    return f"{most_expensive.brand} is the most expensive laptop available for {most_expensive.price}$!"
+# def show_the_most_expensive_laptop():
+#     most_expensive = Laptop.objects.order_by('-price', '-id').first()
+#     return f"{most_expensive.brand} is the most expensive laptop available for {most_expensive.price}$!"
 
-def bulk_create_laptops(*args):
-    Laptop.objects.bulk_create(*args)
-
-
-def update_to_512_GB_storage():
-   Laptop.objects.filter(brand__in=['Asus', 'Lenovo']).update(storage=512)
+# def bulk_create_laptops(*args):
+#     Laptop.objects.bulk_create(*args)
 
 
-def update_to_16_GB_memory():
-    Laptop.objects.filter(brand__in=['Apple', 'Dell', 'Acer']).update(memory=16)
+# def update_to_512_GB_storage():
+#    Laptop.objects.filter(brand__in=['Asus', 'Lenovo']).update(storage=512)
 
 
-def update_operation_systems():
-    Laptop.objects.update(
-        operation_system=Case(
-            When(brand='Asus', then=Value('Windows')),
-            When(brand='Apple', then=Value('MacOS')),
-            When(brand='Dell', then=Value('Linux')),
-            When(brand='Acer',then=Value('Linux')),
-            When(brand='Lenovo', then=Value('Chrome OS'))
-        )
-    )
+# def update_to_16_GB_memory():
+#     Laptop.objects.filter(brand__in=['Apple', 'Dell', 'Acer']).update(memory=16)
 
 
-def delete_inexpensive_laptops():
-    Laptop.objects.filter(price__lt=1200).delete()
+# def update_operation_systems():
+#     Laptop.objects.update(
+#         operation_system=Case(
+#             When(brand='Asus', then=Value('Windows')),
+#             When(brand='Apple', then=Value('MacOS')),
+#             When(brand='Dell', then=Value('Linux')),
+#             When(brand='Acer',then=Value('Linux')),
+#             When(brand='Lenovo', then=Value('Chrome OS'))
+#         )
+#     )
+
+
+# def delete_inexpensive_laptops():
+#     Laptop.objects.filter(price__lt=1200).delete()
+
+###########################################################################
+
+def bulk_create_chess_players(*args):
+    ChessPlayer.objects.bulk_create(*args)
+
+
+def delete_chess_players():
+    ChessPlayer.objects.filter(title="no title").delete()
+
+
+def change_chess_games_won():
+    ChessPlayer.objects.filter(title="GM").update(games_won=30)
+
+
+def change_chess_games_lost():
+    ChessPlayer.objects.filter(title="no title").update(games_lost=25)
+
+
+def change_chess_games_drawn():
+    ChessPlayer.objects.all().update(games_drawn=10)
+
+
+def grand_chess_title_GM():
+    ChessPlayer.objects.filter(rating__gte=2400).update(title="GM")
+
+
+def grand_chess_title_IM():
+    ChessPlayer.objects.filter(Q(rating__gte=2300) & Q(rating__lt=2400)).update(title="IM")
+
+
+def grand_chess_title_FM():
+    ChessPlayer.objects.filter(Q(rating__gte=2200) & Q(rating__lt=2300)).update(title="FM")
+
+
+def grand_chess_title_regular_player():
+    ChessPlayer.objects.filter(rating__lt=2200).update(title="regular player")
