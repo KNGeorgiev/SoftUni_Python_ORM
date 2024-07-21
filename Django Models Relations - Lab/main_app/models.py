99479d6ac7df519fd8e94f0ac2d69173e441bada
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-07-21 09:03:08 +0300

    Second

--- Django Models Relations - Lab/main_app/models.py
+++ Django Models Relations - Lab/main_app/models.py
@@ -22,3 +22,12 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    email = models.EmailField(unique=True)
+    subjects = models.ManyToManyField(to=Subject)
