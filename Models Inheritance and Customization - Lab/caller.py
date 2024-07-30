fa1fb5ad63d5dbcce4774f4d3473de9aef418c34
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-07-30 21:39:39 +0300

    final 07

--- Models Inheritance and Customization - Lab/caller.py
+++ Models Inheritance and Customization - Lab/caller.py
@@ -77,3 +77,12 @@ from main_app.models import Animal, Mammal, Bird, Reptile, Veterinarian, ZooKeep
 # print(f"The snake's age is {snake.age}.")
 
 ########################################################################################################################################
+
+# from main_app.models import Veterinarian
+
+# v1 = Veterinarian.objects.create(first_name="John", last_name="Doe", phone_number="0896625120", license_number="VET123", availability=False)
+# print(v1.availability)
+# v2 = Veterinarian.objects.create(first_name="Alice", last_name="Johnson", phone_number="0896529728", license_number="VET789")
+# print(v2.availability)
+
+########################################################################################################################################
