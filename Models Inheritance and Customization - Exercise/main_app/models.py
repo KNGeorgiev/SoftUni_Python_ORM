d22e2a12891f8f33258a1698b38a51c54be64295
KNGeorgiev <kalin.georgiev.232@gmail.com>
2024-08-02 11:12:20 +0300

    2

--- Models Inheritance and Customization - Exercise/main_app/models.py
+++ Models Inheritance and Customization - Exercise/main_app/models.py
@@ -41,3 +41,39 @@ class VengeanceDemonHunter(DemonHunter):
 
 class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+class Message(models.Model):
+    sender = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self):
+        self.is_read = True
+    
+    def reply_to_message(self, reply_content: str):
+        message = Message(
+            sender = self.receiver,
+            receiver = self.sender,
+            content = reply_content
+        )
+        message.save()
+
+        return message
+
+    def forward_message(self, receiver: UserProfile):
+        message = Message(
+            sender=self.receiver,
+            receiver=receiver,
+            content=self.content,
+        )
+        message.save()
+        
+        return message
